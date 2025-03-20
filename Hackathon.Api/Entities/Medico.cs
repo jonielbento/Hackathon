f895516b14f29d9316b0ba698abe4b38c9895b11
Joniel <57368054+jonielbento@users.noreply.github.com>
2025-03-19 23:59:56 -0300

    Commit entitie medicos

---
+++ Hackathon.Api/Entities/Medico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hackathon.Api.Entities
+{   
+
+    public class Medico
+    {
+        [Key]
+        public long IdMedico { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required, MaxLength(9)]
+        public string Crm { get; set; } = string.Empty;
+
+        public MedicoLogin? MedicoLogin { get; set; }
+        public ICollection<MedicoEspecialidade> MedicoEspecialidades { get; set; } = new List<MedicoEspecialidade>();
+        public ICollection<MedicoHorarioTrabalho> HorariosTrabalho { get; set; } = new List<MedicoHorarioTrabalho>();
+        public ICollection<Consulta> Consultas { get; set; } = new List<Consulta>();
+    }
+}
